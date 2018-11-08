a6963a7f11ea5bff0bb654c675e609f68a30832a
Frank Rügamer <frankruegamer@gmail.com>
2018-11-08 14:07:08 +0100

    Initial commit

---
+++ git-fzf.plugin.zsh
@@ -0,0 +1,50 @@
+_fzf_complete_ga() {
+    _fzf_complete "--multi" "$@" < <(
+        git status -s | awk '/^.\S/'
+    )
+}
+
+_fzf_complete_ga_post() {
+    awk '{print $2}'
+}
+
+_fzf_complete_gco() {
+    _fzf_complete "--multi" "$@" < <(
+        git status -s | awk '/^.\S/'
+        git branch --all --sort=-committerdate | sed 's/\(..\)/ \1/'
+    )
+}
+
+_fzf_complete_gco_post() {
+    sed "s/.* //" | sed "s#remotes/[^/]*/##"
+}
+
+_fzf_complete_grh() {
+    _fzf_complete "--multi" "$@" < <(
+        git status -s | awk '/^[^ ?]/'
+    )
+}
+
+_fzf_complete_grh_post() {
+    awk '{print $2}'
+}
+
+_fzf_complete_gd() {
+    _fzf_complete "--multi" "$@" < <(
+        git status -s | awk '/^.\S/'
+    )
+}
+
+_fzf_complete_gd_post() {
+    awk '{print $2}'
+}
+
+_fzf_complete_gdca() {
+    _fzf_complete "--multi" "$@" < <(
+        git status -s | awk '/^[^ ?]/'
+    )
+}
+
+_fzf_complete_gdca_post() {
+    awk '{print $2}'
+}
