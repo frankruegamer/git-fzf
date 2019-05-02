ae5da8d352580457531c4ade0ab025216559bf83
Frank Rügamer <frankruegamer@gmail.com>
2019-05-02 12:24:48 +0200

    Replace sed slashes with pipes

--- git-fzf.plugin.zsh
+++ git-fzf.plugin.zsh
@@ -11,13 +11,13 @@ _fzf_complete_ga_post() {
 _fzf_complete_gco() {
     _fzf_complete "--multi" "$@" < <(
         git status -s | awk '/^.\S/'
-        git branch --all --sort=-committerdate | sed 's/^/ /'
-        git tag | sed 's/^/ t /'
+        git branch --all --sort=-committerdate | sed 's|^| |'
+        git tag | sed 's|^| t |'
     )
 }
 
 _fzf_complete_gco_post() {
-    sed "s/.* //" | sed "s#remotes/[^/]*/##"
+    sed "s|.* ||" | sed "s|remotes/[^/]*/||"
 }
 
 _fzf_complete_grh() {
